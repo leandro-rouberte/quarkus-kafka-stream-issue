872ce752ffcd776a2510b23f3b7a8b9be65b0393
leandro.roubert <leandro.roubert@zwift.com>
2020-07-14 14:23:11 -0300

    Upgrade quarkus 1.6.0

--- src/main/java/org/acme/TopologyProducer.java
+++ src/main/java/org/acme/TopologyProducer.java
@@ -1,15 +1,12 @@
 package org.acme;
 
-import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
-import org.codehaus.plexus.personality.plexus.lifecycle.phase.Suspendable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
 
 @ApplicationScoped
 public class TopologyProducer {
